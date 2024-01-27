d37c07cd766912a968e4a57831ce9ad35eae7469
Nathan Emonson <nemonson2024@ravenscroft.org>
2024-01-26 21:16:21 -0500

    not working first robothardware

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/COMMON/RobotHardware.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.COMMON;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+public class RobotHardware {
+
+    public HardwareMap hardwareMap;
+    //Drivetrain
+    public DcMotorEx dtFrontRightMotor;
+    public DcMotorEx dtBackRightMotor;
+    public DcMotorEx dtFrontLeftMotor;
+    public DcMotorEx dtBackLeftMotor;
+
+    //Lift
+    public DcMotorEx LeftSlide;
+    public DcMotorEx RightSlide;
+
+    //Intake Motor
+    public DcMotorEx Intake;
+
+    //Outake + Claw
+    public Servo Claw;
+    public Servo ClawPivotLeft;
+    public Servo ClawPivotRight;
+
+    public Servo MiniArmLeft;
+    public Servo MiniArmRight;
+
+
+
+
+    public void RobotHardware(){
+        dtFrontRightMotor = hardwareMap.get(DcMotorEx.class, "dtFrontRightMotor");
+        dtFrontLeftMotor = hardwareMap.get(DcMotorEx.class, "dtFrontLeftMotor");
+        dtBackRightMotor = hardwareMap.get(DcMotorEx.class, "dtBackRightMotor");
+        dtBackLeftMotor = hardwareMap.get(DcMotorEx.class, "dtBackLeftMotor");
+
+        Intake = hardwareMap.get(DcMotorEx.class, "IntakeMotor");
+
+        LeftSlide = hardwareMap.get(DcMotorEx.class, "LeftSlide");
+        RightSlide = hardwareMap.get(DcMotorEx.class, "RightSlide");
+
+        Claw = hardwareMap.get(Servo.class, "Claw");
+        ClawPivotLeft = hardwareMap.get(Servo.class,"ClawPivotLeft");
+        ClawPivotRight = hardwareMap.get(Servo.class,"ClawPivotRight");
+
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -201,10 +201,10 @@ public final class MecanumDrive {
 
         // TODO: make sure your config has motors with these names (or change them)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
-        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "dtFrontLeftMotor");
+        leftBack = hardwareMap.get(DcMotorEx.class, "dtBackleftMotor");
+        rightBack = hardwareMap.get(DcMotorEx.class, "dtBackRightMotor");
+        rightFront = hardwareMap.get(DcMotorEx.class, "dtFrontRightMotor");
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
