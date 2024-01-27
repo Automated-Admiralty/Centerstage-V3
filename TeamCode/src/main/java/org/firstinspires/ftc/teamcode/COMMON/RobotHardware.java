d19a48fac4bdb52befebed46381cc008428de533
Ninja-taxi <nemonson2024@ravenscroft.org>
2024-01-27 15:51:44 -0500

    driving sorta robot hardware working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/COMMON/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/COMMON/RobotHardware.java
@@ -1,6 +1,12 @@
 package org.firstinspires.ftc.teamcode.COMMON;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class RobotHardware {
 
     public HardwareMap hardwareMap;
@@ -25,26 +31,39 @@ public class RobotHardware {
     public Servo MiniArmLeft;
     public Servo MiniArmRight;
 
+    public IMU imu;
 
 
 
-    public void RobotHardware(){
+    public RobotHardware(HardwareMap hardwareMap){
         dtFrontRightMotor = hardwareMap.get(DcMotorEx.class, "dtFrontRightMotor");
         dtFrontLeftMotor = hardwareMap.get(DcMotorEx.class, "dtFrontLeftMotor");
         dtBackRightMotor = hardwareMap.get(DcMotorEx.class, "dtBackRightMotor");
         dtBackLeftMotor = hardwareMap.get(DcMotorEx.class, "dtBackLeftMotor");
 
+        dtFrontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        dtBackLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
         Intake = hardwareMap.get(DcMotorEx.class, "IntakeMotor");
+        Intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Intake.setDirection(DcMotorSimple.Direction.REVERSE);
 
         LeftSlide = hardwareMap.get(DcMotorEx.class, "LeftSlide");
         RightSlide = hardwareMap.get(DcMotorEx.class, "RightSlide");
-
+        LeftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LeftSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RightSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+/*
         Claw = hardwareMap.get(Servo.class, "Claw");
         ClawPivotLeft = hardwareMap.get(Servo.class,"ClawPivotLeft");
         ClawPivotRight = hardwareMap.get(Servo.class,"ClawPivotRight");
         MiniArmLeft = hardwareMap.get(Servo.class,"MiniArmLeft");
         MiniArmRight = hardwareMap.get(Servo.class,"MiniArmRight");
 
+
+ */
+       // imu = hardwareMap.get(IMU.class, "imu");
 
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OPMODE/Tele.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.OPMODE;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.COMMON.RobotHardware;
+
+@TeleOp
+public class Tele extends LinearOpMode {
+
+    // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RobotHardware Robot = new RobotHardware(hardwareMap);
+
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers.
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY -rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            Robot.dtFrontLeftMotor.setPower(frontLeftPower);
+            Robot.dtBackLeftMotor.setPower(backLeftPower);
+            Robot.dtFrontRightMotor.setPower(-frontRightPower);
+            Robot.dtBackRightMotor.setPower(-backRightPower);
+        }
+    }
+}
+
+
